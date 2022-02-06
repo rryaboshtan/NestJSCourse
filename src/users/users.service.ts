3ae286533a7aaa3b01dac99e170758692303d10f
rryaboshtan <rrvworkroman@gmail.com>
2022-02-06 04:11:37 +0200

    use set method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,7 +14,8 @@ export class UsersService {
    async createUser(dto: CreateUserDto) {
       const user = await this.userRepository.create(dto);
       const role = await this.roleService.getRoleByValue('USER');
-      await user.$set('roles', [role]);
+       await user.$set('roles', [role]);
+       
       return user;
    }
 
