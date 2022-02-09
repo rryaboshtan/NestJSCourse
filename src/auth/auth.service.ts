4c95e2bfe0aa51cf9b17af9550b961920a6476f7
rryaboshtan <rrvworkroman@gmail.com>
2022-02-09 23:51:11 +0200

    Auth works

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcryptjs';
+import { User } from 'src/users/users.model';
 
 @Injectable()
 export class AuthService {
@@ -15,6 +16,15 @@ export class AuthService {
       if (candidate) {
          throw new HttpException('User with such email is already exist', HttpStatus.BAD_REQUEST);
       }
-       const hashPassword = await bcrypt.hash(dto.password, 5);
+      const hashPassword = await bcrypt.hash(dto.password, 5);
+      const user = await this.userService.createUser({ ...dto, password: hashPassword });
+      return this.generateToken(user);
+   }
+
+   async generateToken(user: User) {
+      const payload = { email: user.email, id: user.id, roles: user.roles };
+      return {
+         token: this.jwtService.sign(payload),
+      };
    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -16,7 +16,7 @@ export class UsersService {
       const user = await this.userRepository.create(dto);
       const role = await this.roleService.getRoleByValue('USER');
       await user.$set('roles', [role]);
-
+      user.roles = [role];
       return user;
    }
 
